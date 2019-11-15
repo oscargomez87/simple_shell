8c152c55c89d88ee259ce82e5166c0220101f997
Leo CJ <53725367+leocjj@users.noreply.github.com>
2019-11-15 15:36:55 -0500

    v00

---
+++ shs.h
@@ -0,0 +1,13 @@
+#ifndef SHS_H_INCLUDED
+#define SHS_H_INCLUDED
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <string.h>
+
+
+
+#endif
