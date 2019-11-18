1142196c757b4a909e06a7fec8a9122774f5592f
Leo CJ <53725367+leocjj@users.noreply.github.com>
2019-11-18 15:24:06 -0500

    v01 updated

---
+++ back/shs.h
@@ -0,0 +1,16 @@
+#ifndef SHS_H_INCLUDED
+#define SHS_H_INCLUDED
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <string.h>
+
+void _ntty(char *);
+void _itty(char *);
+void _read(char **);
+
+#endif
