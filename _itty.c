25c21e33c43bcd00595d435382c8f9a75a8dc36b
oscargomez87 <oscarstiven87@gmail.com>
2019-11-19 22:36:23 +0000

    Reworked command search so PATH is used removed use of stat function

--- _itty.c
+++ _itty.c
@@ -8,28 +8,20 @@
  */
 void _itty(char *argv)
 {
-	char *command_line = NULL, *env, *saveptr;
+	char *command_line = NULL;
 	char *args_for_execve[] = { "", NULL}, *new_env_vars[] = { NULL };
-	int file_found = -1, file_access = -1, wait_status;
-	stat_t *head;
-	struct stat *st;
+	int file_access = -1, wait_status;
 	pid_t child_pid;
 
 	while (1)
 	{
 		printf("#MiniShell$ ");
 		_read(&command_line);
-		env = _getenv("PATH");
-		while (file_found = stat(command_line, &st) < 0)
-		/* /\* { *\/ */
-		/* /\* 	if (stat(strtok_r(env, ":", &saveptr), &st) == 0) *\/ */
-		/* /\* 		file_found = 0; *\/ */
-		/* /\* 	env =  */ */saveptr;
-		}
-		file_access = access(command_line, F_OK | X_OK);
-		if (file_found == 0)
+		file_access = _findcmd(&command_line, argv);
+		if (file_access == 0)
 		{
-			if (file_access >= 0)
+			file_access = access(command_line, X_OK);
+			if (file_access == 0)
 			{
 				child_pid = fork();
 				if (child_pid == -1)
@@ -45,8 +37,5 @@ void _itty(char *argv)
 				printf("-%s: %s: Permission denied\n",
 				       argv, command_line);
 		}
-		else
-			printf("-%s: %s: No such file or directory\n",
-			       argv, command_line);
 	}
 }
