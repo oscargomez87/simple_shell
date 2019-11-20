f31f8178e3ea8adccb06180fd2bc202ea6fef44d
oscargomez87 <oscarstiven87@gmail.com>
2019-11-20 00:36:58 +0000

    Added conditional when command has a directory syntax and is not found

--- _findcmd.c
+++ _findcmd.c
@@ -1,11 +1,25 @@
 #include "shs.h"
 
+/**
+ * _findcmd - finds the command adquired from input,
+ * searching PATH in case is not a full path
+ *
+ * @command_line: command to find.
+ * @argv: Name of program, used in error printing
+ * Return: 0 if command is found, -1 otherwise
+ */
 int _findcmd(char **command_line, char *argv)
 {
 	char *env, *env_token, *command = NULL;
 
 	if (access(*command_line, F_OK) == 0)
 		return (0);
+	if (**command_line == '/')
+	{
+		printf("%s: %s: No such file or directory\n",
+		       argv, *command_line);
+		return (-1);
+	}
 	env = _getenv("PATH");
 	env_token = strtok(env, ":");
 	while (env_token != NULL)
@@ -24,7 +38,7 @@ int _findcmd(char **command_line, char *argv)
 		free(command);
 		env_token = strtok(NULL, ":");
 	}
-	printf("-%s: %s: No such file or directory\n",
+	printf("%s: %s: No such file or directory\n",
 	       argv, *command_line);
 	return (-1);
 }
